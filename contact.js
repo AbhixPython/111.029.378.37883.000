d8fae39361fcf7fd750692432ac231cffe4b620f
THE FLASKER BOY <161293129+AbhixPython@users.noreply.github.com>
2025-04-26 09:13:22 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-// Toggle navigation menu on hamburger click
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("nav-menu");
-
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
-    });
-});
